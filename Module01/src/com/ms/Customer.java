c4271c480c41450d4fdf09dcb6d0f2eda98c8f76
zyy_glb <1406299849@qq.com>
2019-03-06 22:35:25 +0800

    首次提交

---
+++ Module01/src/com/ms/Customer.java
@@ -0,0 +1,11 @@
+package com.ms;
+
+/**
+ * date:2019/3/6    22:33
+ * author:赵艳阳
+ */
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
---
+++ Module01/src/com/ms/Person.java
@@ -0,0 +1,8 @@
+package com.ms;
+
+/**
+ * date:2019/3/6    22:34
+ * author:赵艳阳
+ */
+public class Person {
+}
